f1e28b303ef8a5a784118cf0d7d9ae8ee5b751b5
Cristina Rodriguez <yosola@gmail.com>
2022-12-03 17:00:57 -0800

    details

--- client/src/components/student.jsx
+++ client/src/components/student.jsx
@@ -6,7 +6,9 @@ const Student = (props) => {
   return (
 
 
-    <div>Place Holder for the info about {props.student.firstname}</div>
+    <div>Place Holder for the info about {props.student.firstname}
+    <p>TO-DO: Form for adding tags | The tag form should grab the date</p>
+    </div>
   )
 }
 
